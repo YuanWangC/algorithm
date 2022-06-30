4d5390c3f35ad91214fb7e93e80ba1bf662789d7
YuanWangC <958268052@qq.com>
2022-06-30 16:24:22 +0800

    Double pointer into o(nlogn)

--- Basic/ThreeSum.java
+++ Basic/ThreeSum.java
@@ -9,33 +9,38 @@ public class ThreeSum {
     List<List<Integer>> res;
 
     public List<List<Integer>> threeSum(int[] nums) {
-        res = new ArrayList<>();
+        if(nums.length>256)res = new ArrayList<>(256);
+        else res = new ArrayList<>(nums.length);
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0)break;
-            int sum = -nums[i];
-            int[] p = search(nums,i,sum);
-            System.out.println(Arrays.toString(p));
-            if(p.length ==0)continue;
-            List<Integer> path = new ArrayList<>();
-            path.add(nums[i]);
-            path.add(p[0]);
-            path.add(p[1]);
-            res.add(path);
-        }
-        return res;
-    }
-
-    private int[] search(int[] nums,int index, int sum) {
-        for(int i=index+1;i<nums.length;i++){
-            if(nums[i]>sum)break;
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j]+nums[i]>sum)break;
-                if(nums[j]+nums[i]==sum)return new int[]{nums[i],nums[j]};
+        if(nums.length<3 || nums[0]>0 || nums[nums.length-1]<0)return res;
+        // System.out.println(Arrays.toString(nums));
+        for(int k=0;k<nums.length-2;k++){
+            if(k>0 && nums[k] == nums[k-1])continue;
+            int i=k+1;
+            int j=nums.length-1;
+            int sum =-1;
+            while(i<j){
+                sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    i++;
+                }
+                else if(sum>0){
+                    j--;
+                }
+                else{
+                    ArrayList path = new ArrayList();
+                    path.add(nums[i]);
+                    path.add(nums[j]);
+                    path.add(nums[k]);
+                    res.add(path);
+                    i++;
+                    j--;
+                    while(i<j && nums[i] == nums[i-1])i++;
+                    while(i<j && nums[j] == nums[j+1])j--;
+                }
             }
         }
-        return new int[0];
+        return res;
     }
 
     public static void main(String[] args){
@@ -46,7 +51,6 @@ public class ThreeSum {
         for(int i=0;i<num.length;i++)
             num[i] = Integer.parseInt(arr[i]);
         List<List<Integer>> result = obj.threeSum(num);
-        System.out.println("run here");
         for(List<Integer> r:result)
             System.out.print(r.toString());
         System.out.println();
