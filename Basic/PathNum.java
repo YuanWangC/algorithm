049741f0e2a7c66053bb8d1a6f32c6fbc8a82154
YuanWangC <958268052@qq.com>
2022-07-18 22:01:09 +0800

    BFS method update

--- Basic/PathNum.java
+++ Basic/PathNum.java
@@ -1,42 +1,92 @@
 package Basic;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class PathNum {
-    public int uniquePathsWithObstacles(int[][] m) {
-        if(m[0][0] == 0 || m[m.length-1][m[0].length-1]==0)return 0;
-        int[][] dp = new int[m.length][m[0].length];
-        dp[0][0] = 1;
-
-        for(int i=1;i<m.length;i++)
-            if(m[i][0] == 0)dp[i][0] = 0;
-            else dp[i][0] = dp[i-1][0];
-
-        for(int i=1;i<m[0].length;i++)
-            if(m[0][i] == 0)dp[0][i] = 0;
-            else dp[0][i] = dp[0][i-1];
-
-        for(int i=1;i<m.length;i++){
-            for(int j=1;j<m[0].length;j++){
-                if(m[i][j]==0)dp[i][j]=0;
-                else dp[i][j] = dp[i][j-1]+dp[i-1][j];
+    HashMap<Integer,Integer> memo = new HashMap<>();
+    int min = Integer.MAX_VALUE;
+    boolean[][] visited;
+    int[][] direct = {{0,1},{0,-1},{1,0},{-1,0}};
+    public void dfs(int[][] m,int i,int j,int path){
+        if(i<0 || i>=m.length || j<0 || j>=m[0].length)return;
+        if(visited[i][j])return;
+        if(m[i][j] == 0)return;
+        if (i == m.length - 1 && j == m[0].length - 1) {
+            if(path<=min){
+                if(!memo.containsKey(path))
+                    memo.put(path,1);
+                else memo.put(path,memo.get(path)+1);
+                min = path;
             }
+            return;
         }
-        return dp[m.length-1][m[0].length-1];
+        visited[i][j] = true;
+        path++;
+        for(int[] d:direct){
+            dfs(m,i+d[0],j+d[1],path);
+        }
+        visited[i][j] = false;
+    }
+    int count = 0;
+    int path = 0;
+    public void bfs(int[][] m){
+        ArrayDeque<int[]> que = new ArrayDeque<>();
+        if(m[0][0] == 0)return;
+        que.offer(new int[]{0,0});
+        while(!que.isEmpty()){
+            int size = que.size();
+            for(int i=0;i<size;i++){
+                int[] cur = que.poll();
+                if(cur[0] == m.length-1 && cur[1] == m[0].length-1){
+                    this.count++;
+                }
+                for(int[] d:direct){
+                    int nx = cur[0]+d[0];
+                    int ny = cur[1]+d[1];
+                    if(nx<0 || ny<0 || nx>=m.length|| ny>=m[0].length)continue;
+                    if(m[nx][ny] == 0)continue;
+                    que.offer(new int[]{nx,ny});
+                }
+            }
+            if(this.count!=0){
+                return;
+            }
+            this.path++;
+        }
+        if(this.count == 0)this.path = 0;
+    }
+    public void uniquePathsWithObstacles(int[][] m) {
+        if(m[0][0] == 0 || m[m.length-1][m[0].length-1]==0)return;
+        visited = new boolean[m.length][m[0].length];
+//        dfs(m,0,0,0);
+        bfs(m);
     }
 
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] matrix = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                matrix[i][j] = in.nextInt();
+        }
+        if(n==1 && matrix[0][0] == 1){
+            System.out.println(1);
+            System.out.println(1);
+        }
         PathNum obj = new PathNum();
-//        int[][] matrix = {{1,1},{0,1}};
-//        int[][] matrix = {{1,0},{0,1}};
-        int[][] matrix = {{1,1,1,1,1,0,1,0},{0,0,0,1,0,1,0,0},{1,1,1,1,1,0,0,0},{1,0,1,0,0,1,1,0}};
-//        int[][] matrix = {{1}};
-        int res = obj.uniquePathsWithObstacles(matrix);
-        if(res == 0){
-            System.out.println(0);
-            System.out.println(res);
-        }
-        else {
-            System.out.println(matrix.length+matrix[0].length-2);
-            System.out.println(res);
-        }
+        obj.uniquePathsWithObstacles(matrix);
+//        if(obj.min == Integer.MAX_VALUE){
+//            System.out.println(0);
+//            System.out.println(0);
+//        }
+//        else {
+//            System.out.println(obj.min);
+//            System.out.println(obj.memo.get(obj.min));
+//        }
+        System.out.println(obj.path);
+        System.out.println(obj.count);
     }
 }
