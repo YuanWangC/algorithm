3f9efc2d5a6b6f30846e4bb1cc035033ddf3be30
YuanWangC <958268052@qq.com>
2022-07-23 16:53:06 +0800

    island problems and flood algo

--- bfs/NumberOfClosedIslands.java
+++ bfs/NumberOfClosedIslands.java
@@ -1,2 +1,8 @@
-package bfs;public class NumberOfClosedIslands {
+package bfs;
+
+public class NumberOfClosedIslands {
+    public static void main(String[] args){
+        int[][] arr = {{0,1,1,0,0,1,0},{1,1,1,1,1,0,1},{1,0,1,1,0,0,0}};
+
+    }
 }
