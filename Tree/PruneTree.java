c237828744dbcba48515fcd5f4a98997de0d884c
YuanWangC <958268052@qq.com>
2022-07-29 22:46:17 +0800

    update tree postOrder practice

--- Tree/PruneTree.java
+++ Tree/PruneTree.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class PruneTree {
+import java.util.HashMap;
+
+/*
+    剪枝，剪去二叉树中所有节点为0的子树
+ */
+public class PruneTree {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null)return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if(root.val == 0 && root.left == null && root.right == null){
+            root = null;
+        }
+        return root;
+
+    }
+    public static void main(String[] args){
+        PruneTree obj = new PruneTree();
+        String[] arr = {"1","0","1","0","0","0","1"};
+        TreeNode root = new TreeNode();
+        TreeNode.Build(root ,arr);
+        root = obj.pruneTree(root);
+        TreeNode.Pretravel(root);
+    }
 }
