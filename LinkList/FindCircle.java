283302723514b8b465f2903dcfe1ec28cbbfa3d9
YuanWangC <958268052@qq.com>
2022-07-29 22:44:25 +0800

    update LinkedList circle practice

--- LinkList/FindCircle.java
+++ LinkList/FindCircle.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class FindCircle {
+public class FindCircle {
+    int circleLength = 0;
+    public ListNode searchCircleIntro(ListNode head){
+//        有环时返回环的入口，无环时返回null
+        if(head == null || head.next == null)return null;
+        ListNode slow = head,fast =head;
+        slow = slow.next;
+        fast = fast.next.next;
+        while(slow!=fast){
+            if(slow==null || fast == null || fast.next == null)return null;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println(slow.value+" "+fast.value);
+        slow = head;
+//        1.求环的入口
+//        while(slow!=fast){
+//            slow = slow.next;
+//            fast = fast.next;
+//        }
+
+//        2.或者，增添一步计算环的长度
+        while(slow!=fast){
+            circleLength++;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+    public static void main(String[] args){
+        ListNode head = new ListNode();
+        int[] arr = {1,2,3,4,5,6,7,8};
+        head.value = arr[0];
+        ListNode cur = head;
+        for(int i=1;i<arr.length;i++){
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+        cur.next = head.next.next.next;
+//        ListNode.traverse(head);
+        FindCircle obj = new FindCircle();
+        ListNode circle = obj.searchCircleIntro(head);
+        System.out.println(circle.value);
+        System.out.println(obj.circleLength);
+    }
 }
--- LinkList/ListNode.java
+++ LinkList/ListNode.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ListNode {
+
+public class ListNode {
+    int value;
+    ListNode next;
+    public ListNode(){}
+    public ListNode(int v){
+        this.value = v;
+        next = null;
+    }
+    public static void traverse(ListNode head){
+        if(head == null)return;
+        ListNode cur = head;
+        while (cur!=null){
+            System.out.println(cur.value);
+            cur = cur.next;
+        }
+    }
+    public  static void main(String[] args){
+        ListNode head = new ListNode();
+        int[] arr = {1,2,3,4,5};
+        head.value = arr[0];
+        ListNode cur = head;
+        for(int i=1;i<arr.length;i++){
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+        traverse(head);
+    }
 }
