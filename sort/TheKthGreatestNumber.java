cadb2e52ca8ad2b0faf9f349b67f23479a27f4aa
YuanWangC <958268052@qq.com>
2022-08-01 21:15:31 +0800

    sort and stack reviews

--- sort/TheKthGreatestNumber.java
+++ sort/TheKthGreatestNumber.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class TheKthGreatestNumber {
+import java.util.Scanner;
+
+public class TheKthGreatestNumber {
+    int k;
+    public void swap(int[] arr,int i,int j){
+        int temp =  arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public int partition(int[] arr,int left,int right){
+        int more = right;
+        while(left<more){
+            while(left<more && arr[left]<=arr[right]){
+                left++;
+            }
+            while(left<more && arr[more]>=arr[right]){
+                more--;
+            }
+            if(left<more)swap(arr,left,more);
+        }
+        swap(arr,right,more);
+        // System.out.println(Arrays.toString(arr));
+        return more;
+    }
+    public void quicksort(int[] arr,int left,int right){
+        if(left>=right)return;
+        int choseIndex = left+(int)(Math.random()*(right-left+1));
+        swap(arr,choseIndex,right);
+        int pos = partition(arr,left,right);
+        if(pos == arr.length-k)return;
+        quicksort(arr,left,pos-1);
+        quicksort(arr,pos+1,right);
+    }
+    public int findKthLargest(int[] nums, int k) {
+        if(k>nums.length)return -1;
+        this.k = k;
+        quicksort(nums,0,nums.length-1);
+        // System.out.println(Arrays.toString(nums));
+        return nums[nums.length-k];
+    }
+    public static void main(String[] args){
+        TheKthGreatestNumber obj = new TheKthGreatestNumber();
+//        int[] arr = {3,2,1,5,6,4};
+//        int k = 2;
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        int res = obj.findKthLargest(arr,k);
+        System.out.println(res);
+    }
 }
--- stack/ValidParentheses.java
+++ stack/ValidParentheses.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class ValidParentheses {
+import java.util.ArrayDeque;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> stk = new ArrayDeque();
+        char[] sc = s.toCharArray();
+        if(sc.length==0)return false;
+        if(sc.length%2!=0)return false;
+        if(sc[0] != '[' && sc[0] != '(' && sc[0] != '{')return false;
+        int index = 1;
+        stk.push(sc[0]);
+//        遍历直到字符串每一个都被判断过，栈为空不是条件，因为匹配过程中会出现栈空情况
+        while(index<sc.length){
+//            一步一步遍历字符串
+            if((sc[index] == '[' ||sc[index] == '(' || sc[index] == '{')){
+                stk.push(sc[index]);
+            }
+            else{
+//                需要反括号时没有可与之匹配的，false
+                if(stk.isEmpty())return false;
+                char cur = stk.peek();
+                // System.out.println(cur +" "+ sc[index]);
+                if(cur == '[' && sc[index] == ']'){
+                    stk.pop();
+                }
+                else if(cur == '(' && sc[index] == ')'){
+                    stk.pop();
+                }
+                else if(cur == '{' && sc[index] == '}'){
+                    stk.pop();
+                }
+//                其他组合都是false
+                else return false;
+            }
+            index++;
+        }
+        if(stk.isEmpty()) return true;
+        return false;
+    }
+    public static void main(String[] args){
+        ValidParentheses obj = new ValidParentheses();
+        String s = ")))(((";
+        boolean res = obj.isValid(s);
+        System.out.println(res);
+    }
 }
