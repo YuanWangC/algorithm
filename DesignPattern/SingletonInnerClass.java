23f194268639bef336d045f90c7e1c82e10872d5
YuanWangC <958268052@qq.com>
2022-08-02 22:04:06 +0800

    inner class singleton practices

--- DesignPattern/SingletonInnerClass.java
+++ DesignPattern/SingletonInnerClass.java
@@ -1,2 +1,30 @@
-package DesignPattern;public class SingletonInnerClass {
+package DesignPattern;
+
+public class SingletonInnerClass{
+    public static void main(String[] args){
+//        Singleton.getInstance();
+//        Singleton.test();        //没有调用，所以不会初始化内部类
+        System.out.println(Singleton.i);    //Singleton初始化不会导致内部类初始化
+    }
+}
+
+
+class Singleton {
+    private Singleton(){}
+    public static Integer  i = 10;
+    static {
+        System.out.println("Singleton init");
+    }
+    private static class SingletonHolder{
+        private static final Singleton INSTANCE = new Singleton();
+        static {
+            System.out.println("holder init");
+        }
+    }
+    public static Singleton getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+    public static void test(){
+        System.out.println("test");
+    }
 }
