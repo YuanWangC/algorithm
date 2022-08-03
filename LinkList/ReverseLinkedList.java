56fd78546bf1a9430ef777cd40eec1124bcb0cee
YuanWangC <958268052@qq.com>
2022-08-03 22:30:27 +0800

    LinkedList reverse practice

--- LinkList/ReverseLinkedList.java
+++ LinkList/ReverseLinkedList.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ReverseLinkedList {
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        if(cur == null)return head;
+        ListNode n = cur.next;
+        head.next = null;
+        if(n == null)return head;
+        while(n!=null){
+            ListNode n2 = n.next;
+            n.next = cur;
+            cur = n;
+            n = n2;
+        }
+        return cur;
+    }
+    public static void main(String[] args){
+        ReverseLinkedList object= new ReverseLinkedList();
+        int[] arr = {1,2,3,4,5};
+        ListNode head = ListNode.build(arr,new ListNode());
+        ListNode newHead = object.reverseList(head);
+        ListNode.traverse(newHead);
+    }
 }
--- LinkList/ReverseLinkedListRange.java
+++ LinkList/ReverseLinkedListRange.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class ReverseLinkedListRange {
+public class ReverseLinkedListRange {
+//    头插法
+    private synchronized ListNode reverseList(ListNode head, int left, int right) {
+        ListNode dummyhead = new ListNode();
+        dummyhead.next = head;
+        int pos = 1;
+        ListNode pre = dummyhead;
+        ListNode cur = head;
+        while(pos<left && cur!=null){
+            cur = cur.next;
+            pre = pre.next;
+            pos++;
+        }
+//        注意此处，不可以移动cur。cur的next指针在变化，可以有效翻转
+        while (cur!=null && pos<right){
+            ListNode nextNode = cur.next;
+            cur.next = nextNode.next;
+//            头插法，放到pre节点后面
+            nextNode.next = pre.next;
+            pre.next = nextNode;
+            pos++;
+        }
+        return dummyhead.next;
+    }
+    public static void main(String[] args){
+        ReverseLinkedListRange object= new ReverseLinkedListRange();
+        int[] arr = {1,2,3,4,5};
+        int leftpos = 2;
+        int rightpos = 4;
+        ListNode head = ListNode.build(arr,new ListNode());
+        ListNode newHead = object.reverseList(head,leftpos,rightpos);
+        ListNode.traverse(newHead);
+    }
 }
