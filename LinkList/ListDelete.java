498a7b3ad6f05dda24b7f0447fac7d5f6544fae4
YuanWangC <958268052@qq.com>
2022-08-05 21:57:19 +0800

    listedList delete practice

--- LinkList/ListDelete.java
+++ LinkList/ListDelete.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class ListDelete {
+public class ListDelete {
+    public ListNode del1(ListNode head,int target){
+        if(head == null)return head;
+        ListNode cur = head;
+        while(cur.value!=target){
+            cur = cur.next;
+        }
+        if(cur == null)return null;
+        if(cur.next==null){
+            cur = null;
+        }
+        else{
+            ListNode n = cur.next;
+            cur.value = n.value;
+            cur.next = n.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode();
+        int[] arr = {1,2,3};
+        head.value = arr[0];
+        ListNode cur = head;
+        for(int i=1;i<arr.length;i++){
+            cur.next= new ListNode(arr[i]);
+            cur = cur.next;
+        }
+        ListDelete obj = new ListDelete();
+        ListNode c = obj.del1(head,2);
+        while(c!=null){
+            System.out.println(c.value);
+            c= c.next;
+        }
+    }
 }
