60c78e961442b03f248921a3d81e72ad966bcc56
YuanWangC <958268052@qq.com>
2022-08-22 10:40:06 +0800

    other test

--- simulation/CountHeadTail.java
+++ simulation/CountHeadTail.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class CountHeadTail {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CountHeadTail {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        List<Integer> arr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr.add(in.nextInt());
+        }
+        if(arr.size()<=2)return;
+        while(arr.size()>2){
+            List<Integer> temp = new ArrayList<>();
+            int i=0,j=arr.size()-1;
+            while(i<j){
+                temp.add(arr.get(i)+arr.get(j));
+                i++;
+                j--;
+            }
+            if(arr.size()%2!=0)temp.add(arr.get(i));
+            arr = temp;
+        }
+        System.out.println(arr);
+    }
 }
